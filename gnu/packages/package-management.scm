8716207e373c0927656998285ab5f7902bd13b17
Greg Hogan <code@greghogan.com>
2024-10-22 18:09:30 +0000

    gnu: Pin CMake dependency.
    * gnu/packages/emulators.scm (python-keystone-engine, unicorn),
    * gnu/packages/freedesktop.scm (libdecor),
    * gnu/packages/gnome.scm (raider, xffm+),
    * gnu/packages/golang-xyz.scm (go-mvdan-cc-editorconfig),
    * gnu/packages/hardware.scm (liblxi, lxi-tools),
    * gnu/packages/mp3.scm (wavbreaker),
    * gnu/packages/package-management.scm (conan),
    * gnu/packages/pantheon.scm (pantheon-calculator, pantheon-calendar),
    * gnu/packages/plotutils.scm (asymptote),
    * gnu/packages/photo.scm (entangle),
    * gnu/packages/python-graphics.scm (python-pivy),
    * gnu/packages/python-xyz.scm (python-awkward-cpp, python-contourpy,
    python-symengine),
    * gnu/packages/syndication.scm (syndication-domination)
    * gnu/packages/qt.scm (soqt),
    * gnu/packages/vnc.scm (tigervnc-server)
    [native-inputs]: Change cmake to cmake-minimal.
    
    * gnu/packages/bioinformatics.scm (trinityrnaseq)
    [native-inputs]: Change cmake to cmake-minimal.
    [arguments]<#:phases>[install]: Remove deletion of uncreated files.
    
    Change-Id: I6ce8b737d9a21f83caad7b79b591e68a86f45fd0

--- gnu/packages/package-management.scm
+++ gnu/packages/package-management.scm
@@ -1502,7 +1502,7 @@ tools_locations = {
     (native-inputs
      (list autoconf-wrapper
            automake
-           cmake
+           cmake-minimal
            git-minimal
            libtool
            meson
