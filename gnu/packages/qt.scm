d7670b4685bf277c92197c41314862f02ea9dd8c
宋文武 <iyzsong@member.fsf.org>
2025-02-08 13:05:10 +0800

    gnu: qtwebglplugin: Rename to qtwebglplugin-5.
    * gnu/packages/qt.scm (qtwebglplugin): Rename to qtwebglplugin-5.
    
    Change-Id: I1e0740f49af019ab067328b19d2c85ee1e9f3d56

--- gnu/packages/qt.scm
+++ gnu/packages/qt.scm
@@ -2335,7 +2335,7 @@ between the host (QML/C++ application) and the client (HTML/JavaScript
 application).")
     (license (package-license qtbase))))
 
-(define-public qtwebglplugin
+(define-public qtwebglplugin-5
   (package
     (inherit qtsvg-5)
     (name "qtwebglplugin")
