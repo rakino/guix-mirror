627669d67cc9b3531c4c779400f2610ac2d157da
宋文武 <iyzsong@member.fsf.org>
2025-02-08 13:10:14 +0800

    gnu: qtdatavis3d: Rename to qtdatavis3d-5.
    * gnu/packages/qt.scm (qtdatavis3d): Rename to qtdatavis3d-5.
    (python-pyside-2) [inputs]: Adjust accordingly.
    
    Change-Id: I60b88003cd4843c956d6abbae39d2773dbd8f080

--- gnu/packages/qt.scm
+++ gnu/packages/qt.scm
@@ -2887,7 +2887,7 @@ graphs by selecting one of the charts themes.")
     (home-page (package-home-page qtbase))
     (license (package-license qtbase))))
 
-(define-public qtdatavis3d
+(define-public qtdatavis3d-5
   (package
     (inherit qtsvg-5)
     (name "qtdatavis3d")
@@ -5319,7 +5319,7 @@ color-related widgets.")
     (build-system cmake-build-system)
     (inputs
      (list qtbase-5
-           qtdatavis3d
+           qtdatavis3d-5
            qtdeclarative-5
            qtlocation-5
            qtmultimedia-5
