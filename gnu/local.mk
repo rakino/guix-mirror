83b9a90232117fcf264b4290f59f34b93a402e5c
宋文武 <iyzsong@member.fsf.org>
2025-02-10 11:42:55 +0800

    gnu: converseen: Update to 0.12.2.5.
    * gnu/packages/patches/converseen-hide-updates-checks.patch: Remove file.
    * gnu/packages/patches/converseen-hide-non-free-pointers.patch: Remove file.
    * gnu/local.mk (dist_patch_DATA): Remove them.
    * gnu/packages/image.scm (converseen): Update to 0.12.2.5.
    [source]: Remove patches.
    [arguments]: Add patch-source phase to disable auto update and website links.
    
    Change-Id: I9cbdad581803d4b1910afb1115e3b8404b1faa7e

--- gnu/local.mk
+++ gnu/local.mk
@@ -1137,8 +1137,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/combinatorial-blas-io-fix.patch		\
   %D%/packages/patches/compsize-fix-btrfs-progs-compatibility.patch	\
   %D%/packages/patches/containerd-create-pid-file.patch	\
-  %D%/packages/patches/converseen-hide-updates-checks.patch	\
-  %D%/packages/patches/converseen-hide-non-free-pointers.patch	\
   %D%/packages/patches/cool-retro-term-wctype.patch		\
   %D%/packages/patches/coq-autosubst-1.8-remove-deprecated-files.patch		\
   %D%/packages/patches/corectrl-polkit-install-dir.patch	\
