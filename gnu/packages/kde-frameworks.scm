5b5431f35f95faf513f9af2e3375e359b4bc9ce8
宋文武 <iyzsong@member.fsf.org>
2025-05-04 14:10:13 +0800

    gnu: kbookmarks: Update to 6.13.0.
    * gnu/packages/kde-frameworks.scm (kbookmarks): Update to 6.13.0.
    
    Change-Id: Ic61aa8fbb827c3d172eaed315995599da15cb374

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -3168,7 +3168,7 @@ by which applications, and what documents have been linked to which activity.")
 (define-public kbookmarks
   (package
     (name "kbookmarks")
-    (version "6.5.0")
+    (version "6.13.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -3177,7 +3177,7 @@ by which applications, and what documents have been linked to which activity.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1v7vamf5541r28yvb6kc4jwjczcsglfhvia9gh8c3smhwpmbrnlp"))))
+                "1lmjdpk91mg5vq1crxvgwj5p16cz731hbw9mhfpjv39siva567bb"))))
     (build-system cmake-build-system)
     (propagated-inputs
      (list kwidgetsaddons))
