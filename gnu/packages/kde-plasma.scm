3b5fd78edd3db38ddf95c941f3def90f96efaf9b
Zheng Junjie <z572@z572.online>
2025-05-05 17:59:44 +0800

    gnu: layer-shell-qt: Update to 6.3.4.
    * gnu/packages/kde-plasma.scm (layer-shell-qt): Update to 6.3.4.
    
    Change-Id: Ib5f6ef157ca146470fe1683c89f9ac13767b476f

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -1060,14 +1060,14 @@ an elegant and intuitive experience for your tasks and plasmoids.")
 (define-public layer-shell-qt
   (package
     (name "layer-shell-qt")
-    (version "6.1.4")
+    (version "6.3.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/layer-shell-qt-" version ".tar.xz"))
               (sha256
                (base32
-                "0s1jzcfq3cqg87bw40krycimxxqq58h808gc92wqclzgdgfd4fb2"))))
+                "0ik3l9lqqh9s2574swv045zj1kk8fg33zsacd3bfzayk90zrh37q"))))
     (build-system qt-build-system)
     (arguments (list #:qtbase qtbase))
     (native-inputs
