15948de9500451b4ea45bea7ac043942fb9183c7
Zheng Junjie <z572@z572.online>
2025-05-05 22:14:46 +0800

    gnu: kdeplasma-addons: Update to 6.3.4.
    * gnu/packages/kde-plasma.scm (kdeplasma-addons): Update to 6.3.4.
    
    Change-Id: I308d264d5a1284d2dbef3e0c4ff8737459c960e3

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -595,14 +595,14 @@ KDE Frameworks 5 to better interact with the system.")
 (define-public kdeplasma-addons
   (package
     (name "kdeplasma-addons")
-    (version "6.1.4")
+    (version "6.3.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/" name "-" version ".tar.xz"))
               (sha256
                (base32
-                "04lspshznmfm75z32x4npids3wz7hdfi1bscs6iz7yf6a1z9lcwb"))))
+                "19w1d2irbsjgvsmsxrnp63g665nk4n5wdmflwvpxxqm6jvin2dmd"))))
     (build-system qt-build-system)
     (arguments
      (list #:qtbase qtbase
