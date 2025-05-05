579be3987debb0dab355015e55070b04585f2b49
Zheng Junjie <z572@z572.online>
2025-05-05 21:36:28 +0800

    gnu: plasma-systemmonitor: Update to 6.3.4.
    * gnu/packages/kde-plasma.scm (plasma-systemmonitor): Update to 6.3.4.
    
    Change-Id: Ic416efaf0c5f2b03ebc31681bbffd6517211fb81

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -2655,14 +2655,14 @@ vaults.")
 (define-public plasma-systemmonitor
   (package
     (name "plasma-systemmonitor")
-    (version "6.1.4")
+    (version "6.3.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version "/"
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0mz49d61k69prvzsqh361gn2vg18916r093wm9vbqqd0kp94hai6"))))
+                "0pdccl64sazzadlk785qbplnrk6f639v33wm0kip44b828lw9mwr"))))
     (build-system qt-build-system)
     (native-inputs (list extra-cmake-modules))
     (inputs (list ki18n
