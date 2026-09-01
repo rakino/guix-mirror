c52eba18e19f29874cf5afbb1d3f8519b8df6545
Liam Hupfer <liam@hpfr.net>
2026-08-31 22:13:41 -0500

    gnu: emacs-eat: Patch aset usage for Emacs 31.
    * gnu/packages/patches/emacs-eat-fix-dec-line-drawing.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Register it here.
    * gnu/packages/emacs-xyz.scm (emacs-eat): Patch aset usage for Emacs 31.
    [patches]: Use it here.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>
    Merges: guix/guix#10955

--- gnu/local.mk
+++ gnu/local.mk
@@ -1258,6 +1258,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/emacs-deferred-fix-number-of-arguments.patch	\
   %D%/packages/patches/emacs-disable-jit-compilation.patch			\
   %D%/packages/patches/emacs-doc-toc-shell-commands.patch	\
+  %D%/packages/patches/emacs-eat-fix-dec-line-drawing.patch	\
   %D%/packages/patches/emacs-elisp-autofmt-fix-region-send.patch	\
   %D%/packages/patches/emacs-fix-scheme-indent-function.patch	\
   %D%/packages/patches/emacs-futur-skip-broken-interruption-test.patch	\
