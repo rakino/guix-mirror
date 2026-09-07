ae237fd80ba432acee9d6a57109e612e4e7e2d96
Vagrant Cascadian <vagrant@debian.org>
2026-09-07 14:26:00 -0700

    gnu: amneziawg: Update to 3.1.20260828.
    Fixes: guix/guix#10563
    
    * gnu/packages/golang-web.scm (go-github-com-amnezia-vpn-amneziawg-go): Update
    to 3.1.20260828.
    * gnu/packages/vpn.scm (amneziawg-tools, amneziawg-linux-kernel-module):
    Update to 3.1.20260828.

--- gnu/packages/vpn.scm
+++ gnu/packages/vpn.scm
@@ -111,7 +111,7 @@
 (define-public amneziawg-linux-kernel-module
   (package
     (name "amneziawg-linux-kernel-module")
-    (version "1.0.20260329")
+    (version "3.1.20260828")
     (source
      (origin
        (method git-fetch)
@@ -121,7 +121,7 @@
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0ygy87cdmxjb7r6sipkniqi3m0952la2sssissbscnlr6sc6rjs2"))))
+        (base32 "17a0xf11ykq4k311ipjxvpirnkx09dqmjmf93lyhcx5bqpf9z27b"))))
     (build-system linux-module-build-system)
     (arguments
      (list
@@ -160,7 +160,7 @@
 (define-public amneziawg-tools
   (package
     (name "amneziawg-tools")
-    (version "1.0.20260223")
+    (version "3.1.20260812")
     (source
      (origin
        (method git-fetch)
@@ -169,7 +169,7 @@
               (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "05jyfqdbk9km0dahmddif7dghp1zslz458yd2hf18fha456qrii6"))))
+        (base32 "12dwzcskssvkh3izgndhkcv2kqy633qmagl573k5wcfqr3gx4mxr"))))
     (build-system gnu-build-system)
     (arguments
      (list
